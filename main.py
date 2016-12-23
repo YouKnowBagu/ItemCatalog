784111979e0e6dd81b673be880ef3f6810b174af
Sean Sampson <sean.p.sampson@gmail.com>
2016-12-23 04:30:08 -0700

    Implement GAuth

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, redirect, url_for, request
-from sqlalchemy import create_engine, desc
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker, scoped_session
-from database_setup import Base, User, Category, Item
-from forms import RegistrationForm
+from database_setup import Base, Category, Item, User
+from flask import session as login_session
+import random
+import string
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+
+CLIENT_ID = json.loads(open('client_secrets.json','r').read())['web']['client_id']
+APPLICATION_NAME = "Catalog"
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind=engine
 
-DBSession = scoped_session(sessionmaker(autocommit=False,
-	autoflush=False,
-	bind=engine))
+DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-Base.query = DBSession.query_property()
+# Base.query = DBSession.query_property()
 
-app = Flask(__name__)
 # routes:
 # show all
 # /
@@ -37,19 +49,97 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/catalog/')
 def home():
-    categories = Category.query.all()
-    items=Item.query.order_by(desc(Item.created)).limit(10).all()
+    categories = session.query(Category).all()
+    items=session.query(Item).order_by(desc(Item.created)).limit(10).all()
     return render_template('main.html', categories=categories, items=items)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User(form.username.data)
-        session.add(user)
-        flash('Thanks for registering')
-        return redirect("google.com", code=302)
-    return render_template('register.html', form=form)
+@app.route('/login')
+def showLogin():
+	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+	login_session['state'] = state
+	return render_template('login.html', STATE=state)
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     form = RegistrationForm(request.form)
+#     if request.method == 'POST' and form.validate():
+#         user = User(form.username.data)
+#         session.add(user)
+#         flash('Thanks for registering')
+#         return redirect("google.com", code=302)
+#     return render_template('register.html', form=form)
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+	# Validate state token
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state parameter.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	code = request.data
+
+	try:
+		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		oauth_flow.redirect_uri = 'postmessage'
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+		response.headers['Content-Type']='application/json'
+		return response
+
+	access_token = credentials.access_token
+	url=('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+	h = httplib2.Http()
+	result = json.loads(h.request(url, 'GET')[1])
+	if result.get('error') is not None:
+	    response = make_response(json.dumps(result.get('error')), 500)
+	    response.headers['Content-Type'] = 'application/json'
+	    return response
+
+	gplus_id = credentials.id_token['sub']
+
+	if result['user_id']!=gplus_id:
+		response=make_response(json.dumps("Token's user ID doesn't match given user ID."),401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Verify that the access token is valid for this app.
+	if result['issued_to']!=CLIENT_ID:
+		response = make_response(json.dumps("Token's client ID does not match app's."),401)
+		print "Token's client ID does not match app's."
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	stored_credentials = login_session.get('credentials')
+	stored_gplus_id = login_session.get('gplus_id')
+	if stored_credentials is not None and gplus_id==stored_gplus_id:
+		response=make_response(json.dumps('Current user is already connected.'),200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+    # Store the access token in the session for later use.
+	login_session['credentials'] = credentials
+	login_session['gplus_id'] = gplus_id
+
+	userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+	params = {'access_token':credentials.access_token, 'alt':'json'}
+	answer = requests.get(userinfo_url, params=params)
+
+	data = answer.json()
+
+	login_session['username'] = data['name']
+	login_session['picture'] = data['picture']
+	login_session['email'] = data['email']
+
+	output = ''
+	output += '<h1>Welcome, '
+	output += login_session['username']
+	output += '!</h1>'
+	output += '<img src="'
+	output += login_session['picture']
+	output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+	flash("you are now logged in as %s" % login_session['username'])
+	print "done!"
+	return output
+
 
 # @app.route('/catalog/categories')
 # def categoryList():
@@ -76,8 +166,8 @@ def deleteCategory():
 @app.route('/catalog/<string:category_name>')
 def categoryItems(category_name):
     output = ''
-    category = Category.query.filter_by(name=category_name).one()
-    items = Item.query.filter_by(category = category).all()
+    category = session.query(Category).filter_by(name=category_name).one()
+    items = session.query(Item).filter_by(category = category).all()
     return render_template('categoryview.html', category = category, items=items)
 
 @app.route('/catalog/<int:category_id>/items/new')
@@ -99,5 +189,6 @@ def deleteItem():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 8080)
+	app.secret_key = 'wtha3U192fJdNodDBl4CtGsG'
+app.debug = True
+app.run(host = '0.0.0.0', port = 8080)
