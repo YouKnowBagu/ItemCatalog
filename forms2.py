cbc324a83ebe1c13b400ce4cd591ff2e328878b9
Sean <sean.p.sampson@gmail.com>
2017-01-07 08:14:28 -0700

    Delete forms2.py

--- forms2.py
+++
@@ -1,9 +0,0 @@
-from wtforms import Form, BooleanField, StringField, validators, TextField
-from wtforms.widgets import TextArea
-from wtforms_alchemy import ModelForm
-from database_setup import Category
-
-class CategoryForm(ModelForm):
-    class Meta:
-        model = Category
-        include = ['user_id']
